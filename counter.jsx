3346561099955b06beaec43f871ff8382fc0e2bd
blake-swl <seulwoolee93@gmail.com>
2019-07-14 16:35:33 -0700

    [add] enzyme test

---
+++ counter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+  state = {
+    count: 0,
+  }
+
+  increment = () => 
+  this.setState(prevState => {
+    return {
+      count: prevState.count + 1,
+    }
+  })
+  decrement = () => 
+  this.setState(prevState => {
+    return {
+      count: prevState.count - 1,
+    }
+  })
+
+
+  render() {
+    return(
+      <div>
+        <p>Current count: {this.state.count}</p>
+        <button onClick={this.increment} className="increment">Increment count</button>
+        <button onClick={this.decrement} className="decrement">Decrement count</button>
+
+      </div>
+    )
+  }
+}
---
+++ counter.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Counter from './counter';
+import { shallow } from 'enzyme';
+
+describe('Counter component', () => {
+  it('starts with a counte of 0', () => {
+    const wrapper = shallow(<Counter />);
+    const text = wrapper.find('p').text()
+
+    expect(text).toEqual("Current count: 0")
+  })
+
+  it('can increment the count when the button is clicked', () => {
+    const wrapper = shallow(<Counter />);
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Current count: 1')
+  })
+
+  it('can decrement the count when the button is clicked', () => {
+    const wrapper = shallow(<Counter />);
+    const decrementBtn = wrapper.find('button.decrement');
+    decrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Current count: -1')
+  })
+})
+
